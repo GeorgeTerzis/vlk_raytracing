a9cb1cf73862cdf8ce838d5eabf39f9588cb3866
george <giorgos5418@gmail.com>
2026-04-14 16:58:11 +0300

    Changes to be committed: 	new file:   compile_shaders.sh 	modified:   compile_shaders.zsh
     Changes not staged for commit:
    	modified:   vk_deps/tinyexr (modified content, untracked content)

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 # slangc -O3 src/shaders/raytracing_recursive/main.slang -o src/shaders/raytracing_recursive/shader.spv &
 slangc -O3 src/shaders/hw_raytracing/main.slang -o src/shaders/hw_raytracing/shader.spv &
 wait
